ce9eba197d379835bc8e0097fa6da5283ad88129
andreosoft <andreyzagorets@gmail.com>
2021-09-11 15:40:36 +0300

    commit

--- src/config/axios.js
+++ src/config/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 const localStorage = JSON.parse(window.localStorage.getItem('neomedy')) || ''
 
 
-axios.defaults.baseURL = 'http://api.neomedy.com/api'
+axios.defaults.baseURL = 'https://api.neomedy.com/api'
 axios.defaults.headers.common['Authorization'] = localStorage.token
 
 // const config = {
